9a7e3e65b510247b85823aaecc6c3c7d9da34a74
khs <T1900843@partner.tmapmobility.com>
2025-04-04 17:23:41 +0900

    구독 서비스 개발

---
+++ src/test/java/com/example/tddpractice/chap03/ExpiryDateCalculatorTest.java
@@ -0,0 +1,23 @@
+package com.example.tddpractice.chap03;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ExpiryDateCalculatorTest {
+
+    @Test
+    void 만원_납부하면_한달_뒤가_만료일이_됨(){
+        assertExpiryDate(LocalDate.of(2019, 3, 1), 10000, LocalDate.of(2019, 4, 1));
+        assertExpiryDate(LocalDate.of(2019, 5, 5), 10000, LocalDate.of(2019, 6, 5));
+    }
+
+    private void assertExpiryDate(LocalDate billingDate, int payAmount, LocalDate expectedExpiryDate){
+        ExpiryDateCalculator cal = new ExpiryDateCalculator();
+        LocalDate realExpiryDate = cal.calculateExpiryDate(billingDate, payAmount);
+        assertEquals(expectedExpiryDate, realExpiryDate);
+    }
+}
