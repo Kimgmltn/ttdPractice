6e0719c5bcf01d07fbda2c9ab553900ea7b6667b
khs <T1900843@partner.tmapmobility.com>
2025-04-05 18:49:50 +0900

    리포지토리를 가짜 구현으로 사용

--- src/test/java/com/example/tddpractice/chap07/UserRegister.java
+++ src/test/java/com/example/tddpractice/chap07/UserRegister.java
@@ -2,13 +2,20 @@ package com.example.tddpractice.chap07;
 
 public class UserRegister {
     private WeakPasswordChecker passwordChecker;
-    public UserRegister(WeakPasswordChecker passwordChecker) {
+    private UserRepository userRepository;
+    public UserRegister(WeakPasswordChecker passwordChecker, UserRepository userRepository) {
         this.passwordChecker = passwordChecker;
+        this.userRepository = userRepository;
     }
 
     public void register(String id, String pw, String email) {
         if (passwordChecker.checkPasswordWeak(pw)) {
             throw new WeakPasswordException();
         }
+        User user = userRepository.findById(id);
+        if (user != null) {
+            throw new DupldException();
+        }
+        userRepository.save(new User(id, pw, email));
     }
 }
--- src/test/java/com/example/tddpractice/chap07/UserRegisterTest.java
+++ src/test/java/com/example/tddpractice/chap07/UserRegisterTest.java
@@ -5,13 +5,16 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 public class UserRegisterTest {
     private UserRegister userRegister;
     private StubWeakPasswordChecker stubWeakPasswordChecker = new StubWeakPasswordChecker();
+    private MemoryUserRepository fakeRepository = new MemoryUserRepository();
 
     @BeforeEach
     void setUp(){
-        this.userRegister = new UserRegister(stubWeakPasswordChecker);
+        this.userRegister = new UserRegister(stubWeakPasswordChecker, fakeRepository);
     }
 
     @DisplayName("약한 암호면 가입 실패")
@@ -19,9 +22,28 @@ public class UserRegisterTest {
     void weakPassword(){
         stubWeakPasswordChecker.setWeak(true);
 
-        Assertions.assertThrows(WeakPasswordException.class, () -> {
+        assertThrows(WeakPasswordException.class, () -> {
             userRegister.register("id", "pw", "email");
         });
+    }
+
+    @DisplayName("이미 같은 ID가 존재하면 가입 실패")
+    @Test
+    void dupldExists() {
+        fakeRepository.save(new User("id", "pw1", "email@email.com"));
+        assertThrows(DupldException.class, () -> {
+            userRegister.register("id", "pw2", "email");
+        });
+    }
+
+    @DisplayName("같은 ID가 없으면 가입 성공함")
+    @Test
+    void noDupld_RegisterSuccess(){
+        userRegister.register("id","pw","email");
+
+        User savedUser = fakeRepository.findById("id");
+        assertEquals("id",savedUser.getId());
+        assertEquals("email",savedUser.getEmail());
     }
 }
 
